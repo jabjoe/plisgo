b08af0ed7c4c82067b7b5434710fe751722cf7ed
jabjoe <jabjoe@f301a074-b28b-4858-84c8-f8b36c4887ce>
2010-04-07 08:41:58 +0000

    Important "filesystem" flag missing after GetAttributesOf change, was causing issues with properties view.
    git-svn-id: https://svn.code.sf.net/p/plisgo/code@101 f301a074-b28b-4858-84c8-f8b36c4887ce

--- Plisgo/PlisgoFolder.h
+++ Plisgo/PlisgoFolder.h
@@ -240,7 +240,7 @@ public:
 		if (nAttr == INVALID_FILE_ATTRIBUTES)
 			return STG_E_FILENOTFOUND;
 
-		*rgfInOut = SFGAO_ISSLOW | SFGAO_STORAGE;
+		*rgfInOut = SFGAO_ISSLOW | SFGAO_STORAGE | SFGAO_FILESYSTEM;
 
 		if (nAttr&FILE_ATTRIBUTE_DIRECTORY)
 			*rgfInOut |= SFGAO_FOLDER|SFGAO_HASSUBFOLDER; //Too much work to check every folder for sub folders
